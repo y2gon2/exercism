a7495c44e578d89cd86f109aaf5ff8d99efd44e9
gon <y2gon2@gmail.com>
2023-12-29 21:25:57 +0900

    recommandation 반영
    `when is_binary(name)` 의 guard clause 를 적용하면, 그 외 type 의 경우,  run-time 중 자체적으로 FunctionClauseError를 발생시키므로 이를 명시적 구현을 할 필요가 없다.
    
    아울러  `\\ "you"` 와 같이 default 값을 넣어주면  `def two_fer/0` 도 명시적으로 작성할 필요가 없으므로 이를 제거함.

--- lib/ch2_easy/e02_two_fer.ex
+++ lib/ch2_easy/e02_two_fer.ex
@@ -7,8 +7,6 @@ defmodule TwoFer do
   Two-fer or 2-fer is short for two for one. One for you and one for me.
   """
   @spec two_fer(String.t()) :: String.t()
-  def two_fer(name) when is_binary(name), do: "One for #{name}, one for me."
-  def two_fer(name) when is_integer(name), do: raise FunctionClauseError
-  def two_fer(), do: "One for you, one for me."
+  def two_fer(name \\ "you") when is_binary(name), do: "One for #{name}, one for me."
 
 end
